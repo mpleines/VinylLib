4760039924debfbdad47784f3f65102e000304a4
mpleines <maik.pleines@googlemail.com>
2020-05-26 19:18:31 +0200

    add Button Components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { PrimaryButton, SecondaryButton } from "./components/Buttons";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "space-around",
+      }}
+    >
+      <PrimaryButton>Sign Up</PrimaryButton>
+      <SecondaryButton>Log In</SecondaryButton>
     </div>
   );
 }
