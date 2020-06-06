47643a904678b3998d379258aaee6169014f58b9
mpleines <maik.pleines@googlemail.com>
2020-06-06 12:12:16 +0200

    replaced Column and Row with Divider Component

--- src/components/Divider.js
+++ src/components/Divider.js
@@ -6,14 +6,11 @@ export const Divider = styled.div`
   flex-wrap: wrap;
   justify-content: ${(props) =>
     props.justifyContent ? props.justifyContent : "flex-start"};
-  align-items: ${(props) => (props.alignItems ? props.alignItems : null)};
+  align-items: ${(props) => (props.alignItems ? props.alignItems : "null")};
   > * {
-    margin-left: 0.5rem;
-    margin-top: 0.5rem;
     flex: ${(props) => (props.flex ? props.flex : 1)};
     &:not(:first-child) {
-      margin-left: 0.5rem;
-      margin-top: 0.5rem;
+      margin: ${(props) => (props.margin ? props.margin : "0.5rem 0 0 0.5rem")};
     }
   }
 `;
