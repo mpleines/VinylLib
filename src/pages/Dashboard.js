adb5387aed0a428fc82489c0fce82e1b214369b1
mpleines <maik.pleines@googlemail.com>
2020-06-06 12:11:35 +0200

    updated dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Title, Paragraph } from "../components/Fonts";
 import { Divider } from "../components/Divider";
 import { PrimaryButton } from "../components/Buttons";
+import { ReactComponent as HappyBird } from "../icons/happy-bird.svg";
 
 export const Dashboard = () => {
   const styles = {
     dashboard: {
-      padding: "2rem",
+      padding: "3rem",
     },
   };
   return (
@@ -17,6 +18,9 @@ export const Dashboard = () => {
         new Record.
       </Paragraph>
       <PrimaryButton>Add Record</PrimaryButton>
+      <Divider justifyContent="flex-end">
+        <HappyBird />
+      </Divider>
     </div>
   );
 };
