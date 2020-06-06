3d75b2e4b813db31856297f1601db3644e02a804
mpleines <maik.pleines@googlemail.com>
2020-06-06 14:36:47 +0200

    Updated Navigation to use Links of router

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,20 +4,23 @@ import { PrimaryButton } from "./Buttons";
 import { ReactComponent as LpIcon } from "../icons/lp.svg";
 import { ReactComponent as UserIcon } from "../icons/user.svg";
 import { Link } from "./Link";
+import { useHistory } from "react-router-dom";
 
 export const Navigation = () => {
+  const history = useHistory();
   const styles = {
     navigation: {
       padding: "1rem 2rem",
     },
   };
+
   return (
     <div style={styles.navigation}>
       <Divider alignItems="center">
         <Divider alignItems="center" flex="0 0 auto" margin="0 0 0 1rem">
           <LpIcon />
-          <Link href="#">Dashboard</Link>
-          <Link href="#">All Records</Link>
+          <Link to="/dashboard">Dashboard</Link>
+          <Link to="/all-records">All Records</Link>
         </Divider>
         <Divider
           alignItems="center"
@@ -25,7 +28,9 @@ export const Navigation = () => {
           flex="0 0 auto"
           margin="0 0 0 1rem"
         >
-          <PrimaryButton>Add Record</PrimaryButton>
+          <PrimaryButton onClick={() => history.push("/add-record")}>
+            Add Record
+          </PrimaryButton>
           <UserIcon />
         </Divider>
       </Divider>
