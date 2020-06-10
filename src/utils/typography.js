e91c46a9f292be5ddc3ec0992b6253ec987236e7
mpleines <maik.pleines@googlemail.com>
2020-06-10 19:37:28 +0200

    updated type scale

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,10 +1,10 @@
 export const typeScale = {
-  header1: "5.653rem",
-  header2: "3.998rem",
-  header3: "2.827rem",
-  header4: "1.999rem",
-  header5: "1.414rem",
+  header1: "4.209rem",
+  header2: "3.157rem",
+  header3: "2.369rem",
+  header4: "1.777rem",
+  header5: "1.333rem",
   paragraph: "1rem",
-  helper: "0.707rem",
-  copyright: "0.5rem",
+  helper: "0.75rem",
+  copyright: "0.563rem",
 };
