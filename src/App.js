07579be39a284d677b9233b157596940dc741597
mpleines <maik.pleines@googlemail.com>
2020-06-11 20:51:56 +0200

    moved main

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Navigation } from "./components/Navigation";
 import { BrowserRouter as Router } from "react-router-dom";
-import Main from "./Main";
+import Main from "./pages/Main";
 
 function App() {
   return (
