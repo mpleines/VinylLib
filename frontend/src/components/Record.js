66dcd9faf85c66b8813dc01104a3e7f512a09cff
Maik Pleines <maikpleines@MacBook-Pro.fritz.box>
2020-06-14 17:14:45 +0200

    moved frontend to own folder, created api folder

---
+++ frontend/src/components/Record.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from 'styled-components';
+import { H5, Paragraph } from './Fonts';
+import zappa from './zappa.jpg'
+
+const RecordWrapper = styled.div`
+  padding: .5rem;
+  margin: 1.5rem;
+  display: flex;
+  align-items: center;
+  box-shadow: 0 6px 6px rgba(0, 0, 0, 0.2);
+`;
+
+const RecordImageWrapper = styled.div`
+  display: inline-block;
+  position: relative;
+  width: 64px;
+  height: 64px;
+  overflow: hidden;
+`;
+
+const RecordImage = styled.img`
+  width: auto;
+  height: 100%;
+`;
+
+const RecordArt = ({image}) => {
+  return(
+    <RecordImageWrapper>
+      <RecordImage src={image} />
+    </RecordImageWrapper>
+  )
+}
+
+const RecordInfo = styled.div`
+  margin-left: 1.5rem;
+`;
+
+const Record = ({title, description}) => {
+  return (
+    <RecordWrapper>
+      <RecordArt image={zappa} />
+      <RecordInfo>
+        <H5>{title}</H5>
+        <Paragraph>{description}</Paragraph>
+      </RecordInfo>
+    </RecordWrapper>
+  )
+}
+
+export default Record;
