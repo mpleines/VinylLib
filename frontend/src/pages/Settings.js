0f7519dd171582299217f88c3135eed1a5eaf4d8
Maik Pleines <maikpleines@MacBook-Pro.fritz.box>
2020-06-23 19:40:37 +0200

    add logout

--- frontend/src/pages/Settings.js
+++ frontend/src/pages/Settings.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Page } from "../components/Page";
 import { Heading } from "../components/Fonts";
+import { ErrorButton } from "../components/Buttons";
+import UserContext from "../contexts/UserContext";
 
 const Settings = () => {
+  const {user, setUser} = useContext(UserContext);
+
+  const logout = () => {
+    setUser({});
+    // delete bearer token
+    localStorage.removeItem('token');
+  }
+
   return (
     <Page>
       <Heading>Settings</Heading>
+      <ErrorButton onClick={logout}>Logout</ErrorButton>
     </Page>
   );
 };
