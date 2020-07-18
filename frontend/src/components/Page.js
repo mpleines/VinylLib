cdb3b15ce55c067a669fdf13dacc442b9d7a517b
Maik Pleines <maikpleines@MacBook-Pro.fritz.box>
2020-07-18 11:42:12 +0200

    add FullPage component

--- frontend/src/components/Page.js
+++ frontend/src/components/Page.js
@@ -1,7 +1,13 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import { colors } from '../utils/colors';
 
 export const Page = styled.div`
   padding: 0 5rem;
   margin: 0 auto;
   max-width: 900px;
+`;
+
+export const FullPage = styled.div`
+  height: 100vh;
+  background: ${colors.background};
 `;
