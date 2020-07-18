3c67f3b6a05bca81b63a1b16516cfbac70d6d56e
Maik Pleines <maikpleines@MacBook-Pro.fritz.box>
2020-07-18 11:42:33 +0200

    adjust landing page

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,31 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
-import {colors} from '../utils/colors';
+import { colors } from '../utils/colors';
 import { Title, Paragraph, H3 } from '../components/Fonts';
-import { TertiaryButton, SecondaryButton } from '../components/Buttons';
-import { useHistory } from "react-router-dom";
+import { BigPrimaryButton, SecondaryButton } from '../components/Buttons';
+import { useHistory } from 'react-router-dom';
 import Margin from '../components/Margin';
+import Banner from '../components/Banner';
+import Center from '../components/Center';
+import { ReactComponent as LpIcon } from '../icons/lp.svg';
 
 const LandingPage = styled.div`
   height: 100vh;
   background: ${colors.background};
-`;
-
-const Center = styled.div`
-  margin-top: 10em;
-  display: flex; 
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-`;
-
-const Banner = styled.div`
-  background: ${colors.text.normal};
-  filter: saturate(50%);
-  padding: .2em 1.3em;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
 `;
 
 const Landing = () => {
@@ -33,17 +19,25 @@ const Landing = () => {
   return (
     <LandingPage>
       <Banner>
-        <H3 white>VinylLib </H3>
-        <SecondaryButton onClick={() => history.push('/login')}>Login</SecondaryButton>
+        <LpIcon />
+        <H3>VinylLib </H3>
+        <SecondaryButton onClick={() => history.push('/login')}>
+          Login
+        </SecondaryButton>
       </Banner>
       <Center>
-        <Title white noMargin>Your records, ordered.</Title>
-        <Paragraph white>Organize, categorize & store all of your records. Lifetime free.</Paragraph>
-        <Margin margin={'3em'}/>
-        <TertiaryButton onClick={() => history.push('/register')}>Register now for free</TertiaryButton>
+        <Margin margin={'10em'} />
+        <Title noMargin>Your records, ordered.</Title>
+        <Paragraph>
+          Organize, categorize & store all of your records. Lifetime free.
+        </Paragraph>
+        <Margin margin={'3em'} />
+        <BigPrimaryButton onClick={() => history.push('/register')}>
+          Register now for free
+        </BigPrimaryButton>
       </Center>
     </LandingPage>
-  )
-}
+  );
+};
 
 export default Landing;
