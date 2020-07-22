a6cc2723433a71fbe8b70a07dbc645843454e7b3
Maik Pleines <maikpleines@MacBook-Pro.fritz.box>
2020-07-22 20:43:13 +0200

    get token before each route

--- frontend/src/ApiService/ApiService.js
+++ frontend/src/ApiService/ApiService.js
@@ -1,25 +1,15 @@
-export const getJwtOfUser= () => {
-  const jwt = localStorage.getItem('token');
-  return jwt;
-}
-
-const options = {
-  headers : { 
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-    'Authorization': `Bearer ${getJwtOfUser()}`
-  }
-}
-
 // login the user
 export const login = async (user) => {
   const response = await fetch(`${process.env.REACT_APP_API_URL}${'/login'}`, {
     method: 'POST',
-    ...options,
-    body: JSON.stringify(user)
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify(user),
   });
 
-  if(response.status !== 200) {
+  if (response.status !== 200) {
     const res = await response.json();
     const errorMessage = res.message;
     throw new Error(errorMessage);
@@ -27,56 +17,89 @@ export const login = async (user) => {
 
   const { token } = await response.json();
   return token;
-}
+};
 
 // register new User
 export const register = async (newUser) => {
-  const response = await fetch(`${process.env.REACT_APP_API_URL}${'/register'}`, {
-    method: 'POST',
-    ...options,
-    body: JSON.stringify(newUser)
-  });
+  const response = await fetch(
+    `${process.env.REACT_APP_API_URL}${'/register'}`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify(newUser),
+    },
+  );
 
-  if(response.status !== 200) {
+  if (response.status !== 200) {
     const res = await response.json();
     const errorMessage = res.message;
     throw new Error(errorMessage);
   }
-}
+};
 
 // common API requests go here
 export const getRecords = async (user) => {
-  const response = await fetch(`${process.env.REACT_APP_API_URL}${'/records/all'}`, {
-    method: 'POST',
-    body: user,
-    ...options
-  });
+  const token = localStorage.getItem('token');
+
+  const response = await fetch(
+    `${process.env.REACT_APP_API_URL}${'/records/all'}`,
+    {
+      method: 'POST',
+      body: user,
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  );
   const data = await response.json();
   return data;
-}
+};
 
 export const postRecord = async (record) => {
-  const response = await fetch(`${process.env.REACT_APP_API_URL}${'/records'}`, {
-    method: 'POST',
-    body: JSON.stringify(record),
-    ...options
-  });
+  const token = localStorage.getItem('token');
+
+  const response = await fetch(
+    `${process.env.REACT_APP_API_URL}${'/records'}`,
+    {
+      method: 'POST',
+      body: JSON.stringify(record),
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  );
   return response;
-}
+};
 
 export const deleteRecord = async (recordId) => {
-  const response = await fetch(`${process.env.REACT_APP_API_URL}${'/records'}/${recordId}`, {
-    method: 'DELETE',
-    ...options
-  });
+  const token = localStorage.getItem('token');
+
+  const response = await fetch(
+    `${process.env.REACT_APP_API_URL}${'/records'}/${recordId}`,
+    {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  );
   return response;
-}
+};
 
 export const emptyResponseHandler = async (request, body) => {
   const res = await request(JSON.stringify(body));
-  if(Array.isArray(res)) {
+  if (Array.isArray(res)) {
     return res;
   } else {
     return [];
   }
-}
+};
