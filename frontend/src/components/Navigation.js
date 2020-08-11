772f60c9bb225f67a149ed0103b26b26c6c5ab93
Maik Pleines <maikpleines@MacBook-Pro.fritz.box>
2020-08-11 19:10:11 +0200

    restyle navigation

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Divider } from './Divider';
 import { PrimaryButton } from './Buttons';
 import { ReactComponent as LpIcon } from '../icons/lp.svg';
@@ -7,42 +8,52 @@ import { Link } from './Link';
 import { useHistory } from 'react-router-dom';
 import { colors } from '../utils/colors';
 import Logo from '../components/Logo';
+import Margin from '../components/Margin';
+
+const NavigationItemContainer = styled.div`
+  padding: 0.2em 0.7em;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+
+  > * {
+    margin-right: 1.2em;
+  }
+
+  &:last-child {
+    margin: 0;
+  }
+`;
+
+const StyledNavigation = styled.div`
+  top: 0;
+  width: 100%;
+  position: fixed;
+  background: ${colors.text.normal};
+  display: flex;
+  justify-content: space-between;
+`;
 
 export const Navigation = () => {
   const history = useHistory();
-  const styles = {
-    navigation: {
-      position: 'fixed',
-      width: '100%',
-      top: '0',
-      background: colors.text.normal,
-    },
-  };
 
   return (
-    <div style={styles.navigation}>
-      <Divider alignItems="center">
-        <Divider alignItems="center" flex="0 0 auto" margin="0 0 0 1rem">
-          <Link to="/dashboard">
-            <Logo />
-          </Link>
-          <Link to="/dashboard">Dashboard</Link>
-          <Link to="/all-records">All Records</Link>
-        </Divider>
-        <Divider
-          alignItems="center"
-          justifyContent="flex-end"
-          flex="0 0 auto"
-          margin="0 0 0 1rem"
-        >
-          <PrimaryButton onClick={() => history.push('/add-record')}>
-            Add Record
-          </PrimaryButton>
-          <Link to="/settings">
-            <UserIcon />
-          </Link>
-        </Divider>
-      </Divider>
-    </div>
+    <StyledNavigation>
+      <NavigationItemContainer>
+        <Link to="/dashboard">
+          <Logo />
+        </Link>
+        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/all-records">All Records</Link>
+      </NavigationItemContainer>
+      <NavigationItemContainer>
+        <PrimaryButton onClick={() => history.push('/add-record')}>
+          Add Record
+        </PrimaryButton>
+        <Link to="/settings">
+          <UserIcon />
+        </Link>
+      </NavigationItemContainer>
+    </StyledNavigation>
   );
 };
