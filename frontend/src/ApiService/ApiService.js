c66d1c9dc7f8dd4d089d332d66518edc46a15844
Maik Pleines <maikpleines@MacBook-Pro.fritz.box>
2020-09-28 20:12:04 +0200

    testing

--- frontend/src/ApiService/ApiService.js
+++ frontend/src/ApiService/ApiService.js
@@ -8,7 +8,7 @@ const DEFAULT_HEADERS = {
 export const request = async (endpoint, payload, method = 'POST') => {
   const token = localStorage.getItem('token');
   const auth = { Authorization: `Bearer ${token}` };
-  const URI = process.env.REACT_APP_API_URL || 'http://api:8080';
+  const URI = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
   try {
     const response = await axios({
